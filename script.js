1336cc2fd6755250f1d7aa668401ce61fe3e9fac
Brendan Capp <brendan.capp@west-mec.org>
2015-10-30 15:23:24 -0700

    The final product of my Food Tracker Application.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-function AddData() {
-    var x = document.getElementById("Buy Date").value;
-    var y = document.getElementById("name").value;
-    var letters = '/^[a-zA-Z]+$/';
-    if ((parseInt(x) != (x)) && (y == parseInt(y))) {
-        alert("Wrong Value Entered");
-    } else {
-        var rows = "";
-        var name = document.getElementById("name").value;
-        var bd = document.getElementById("Buy Date").value;
-        var ed = document.getElementById("Expiration Date").value;
-        var des = document.getElementById("description").value;
+function addFoods() {
+    var name = $('#name').val();
+    var bd = $('#bd').val();
+    var ed = $('#ed').val();
+    var des = $('#des').val();
 
-        rows += "<td>" + name + "</td><td>" + bd + "</td><td>" + ed + "</td><td>" + des + "</td>";
-        var tbody = document.querySelector("#List tbody");
-        var tr = document.createElement("tr");
+    $('#name').val();
+    $('#bd').val();
+    $('#ed').val();
+    $('#des').val();
 
-        tr.innerHTML = rows;
-        tbody.appendChild(tr);
+    var foodExpiration = Date.parse(ed);
+    var today = new Date();
+    var currentDate = Date.parse(today);
+
+    if (currentDate > foodExpiration - 259200000) {
+        $('#savedFoods').append('<tr style="background-color: red"><td>' + name + '</td><td>' + bd + '</td><td>' + ed + '</td><td>' + des + '</td></tr>');
+    }
+    else {
+        $('#savedFoods').append('<tr><td>' + name + '</td><td>' + bd + '</td><td>' + ed + '</td><td>' + des + '</td></tr>');
     }
 }
-
-function ResetForm() {
-    document.getElementById("Food").reset();
-    document.getElementById("data table").reset();
-}
-
-    if () {}
-
